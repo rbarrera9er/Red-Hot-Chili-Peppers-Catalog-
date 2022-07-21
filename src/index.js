cd1060334d8dbab3f8cf52c58c8b1ec2abf38885
Ryan Barrera <rbarrera9er@gmail.com>
2022-07-20 21:35:14 -0400

    almost done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,58 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     retrieveAlbums()
+    document.getElementById('albums').addEventListener('click', retrieveAlbums)
 })
 
 function retrieveAlbums(){
-    const mainUl = document.getElementById('Album-List')
+    const ul = document.getElementById('Album-List')
+    const band = document.getElementById('band')
+    band.innerHTML = ""
+    ul.innerHTML = ""
     fetch('http://localhost:3000/Albums')
     .then(res => res.json())
     .then(data => {
-        console.log('ryan')
+        data.forEach(Albums => {
+            ul.innerHTML +=
+                `<a href="#" data-id="${Albums.id}">${Albums.title} </a>`
 
+
+        })
+        makeLinksLive() // was orignally on line 19
     })
-    console.log('barrera')
+    
 }
+
+function makeLinksLive(){
+    const Albums = document.querySelectorAll('a')
+    Albums.forEach((Albums) => {
+        Albums.addEventListener('click', showAlbum)
+    })
+
+}
+
+const showAlbum = (event) => {
+    // no default action on a click
+    console.log(event.target.dataset.id)  //click event
+    const band = document.getElementById('band')
+    const ul = document.getElementById('Album-List')
+    ul.innerHTML = ''
+    fetch('http://localhost:3000' + `/Albums/${event.target.dataset.id}`) //this was a pain in the ass... returns promised obj
+    .then(res => res.json())
+    .then(Albums => {
+        console.log(Albums)
+        band.innerHTML = `<h1>${Albums.title}</h1><br/>
+        <h2>Album-Cover</h2>
+        <p>${Albums.url}</p>
+        <h2>Year-Album-Release</h2>
+        <h3>${Albums.year}</h3>
+        <h2>Album-Background-Information</h2>
+        <p>${Albums.description}</p>
+        <h2>Band-Members</h2>
+        <p>${Albums.members}</p>
+        <h2>Songs</h2>
+        <p>${Albums.songs.join(", ")}</p>`
+    })
+}
+
+
