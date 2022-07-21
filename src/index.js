99a8c9188078cc61ecb8b41724caf1c62e775929
Ryan Barrera <rbarrera9er@gmail.com>
2022-07-21 16:33:55 -0400

    95% there

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-
+//const image_input = document.querySelectorAll("#image_input");
 window.addEventListener('DOMContentLoaded', () => {
     retrieveAlbums()
     document.getElementById('albums').addEventListener('click', retrieveAlbums)
-})
+    
+})    
 
 function retrieveAlbums(){
     let ul = document.getElementById('Album-List')
     let band = document.getElementById('band')
+    
     band.innerHTML = ""
     ul.innerHTML = ""
+    .innerHTML= ""
     fetch('http://localhost:3000/Albums')
     .then(res => res.json())
     .then(data => {
@@ -25,6 +28,7 @@ function retrieveAlbums(){
 
 function makeLinksLive(){
     const Albums = document.querySelectorAll('a')
+
     Albums.forEach((Albums) => {
         Albums.addEventListener('click', showAlbum)
     })
@@ -43,7 +47,7 @@ const showAlbum = (event) => {
         console.log(Albums)
         band.innerHTML = `<h1>${Albums.title}</h1><br/>
         <h2>Album-Cover</h2>
-        <p>${Albums.url}</p>
+        <img src="${Albums.url}"/>
         <h2>Year-Album-Release</h2>
         <h3>${Albums.year}</h3>
         <h2>Album-Background-Information</h2>
@@ -55,4 +59,32 @@ const showAlbum = (event) => {
     })
 }
 
+let i = 0;
+let images = [];
+let time = 4000;
+
+    images[0] = 'https://upload.wikimedia.org/wikipedia/en/5/5e/Rhcp1.jpg';
+    images[1] = 'https://upload.wikimedia.org/wikipedia/en/5/5b/Freakystyleyalbumcover.jpg';
+    images[2] = 'https://upload.wikimedia.org/wikipedia/en/a/a2/UpliftMofoCover.jpg';
+    images[3] = 'https://upload.wikimedia.org/wikipedia/en/9/98/Mother%27sMilkAlbumcover.jpg';
+    images[4] = 'https://upload.wikimedia.org/wikipedia/en/5/5e/RHCP-BSSM.jpg';
+    images[5] = 'https://upload.wikimedia.org/wikipedia/en/8/8a/Rhcp7.jpg';
+    images[6] = 'https://upload.wikimedia.org/wikipedia/en/d/df/RedHotChiliPeppersCalifornication.jpg';
+    images[7] = 'https://upload.wikimedia.org/wikipedia/en/2/23/Rhcp9.jpg';
+    images[8] = 'https://upload.wikimedia.org/wikipedia/en/e/e6/Stadiumarcadium.jpg';
+
+function changeImg(){
+    document.slide.src = images[i];
+
+        if(i < images.length - 1){
+           i++;
+        } else{
+        i = 0
+        }
+
+ setTimeout("changeImg()", time);
+
+}
+
+window.onload = changeImg;
 
