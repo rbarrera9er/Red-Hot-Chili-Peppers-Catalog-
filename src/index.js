10dea33e0ed928c1abc6a101d50856e7d49019b1
Ryan Barrera <rbarrera9er@gmail.com>
2022-07-24 21:35:07 -0400

    cleaned JS CSS & HTML a little and removed comments for walk through video

--- src/index.js
+++ src/index.js
@@ -1,71 +1,60 @@
-//const image_input = document.querySelectorAll("#image_input");
 window.addEventListener('DOMContentLoaded', () => {
     retrieveAlbums()
     document.getElementById('albums').addEventListener('click', retrieveAlbums)
     
-})    
+});   
 
 function retrieveAlbums(){
-    let ul = document.getElementById('Album-List')
+    let rhcpList = document.getElementById('Album-List')
     let band = document.getElementById('band')   
     band.innerHTML = ""
-    ul.innerHTML = ""  
+    rhcpList.innerHTML = ""  
     fetch('http://localhost:3000/Albums')
     .then(response => response.json())
     .then(data => {
         data.forEach(Albums => {
-            ul.innerHTML +=
-               `<div class="item1"><a href="#" data-id="${Albums.id}">${Albums.title} </a></div>`
+            rhcpList.innerHTML +=
+               `<div class="item1"><a href="#" data-id="${Albums.id}">${Albums.title} </a> </div>`
         })
-        activateLinks() // was orignally on line 19
-    })    
+        activateLinks() 
+    });    
 }
 
 function activateLinks(){
     const Albums = document.querySelectorAll('a')
-   // const slideshow = document.getElementsByClassName("slideshow-container");
-    //slideshow[0].style.display = "none";
     Albums.forEach((Albums) => {
-        Albums.addEventListener('click', showAlbum)
-        
-    })
-    
+        Albums.addEventListener('click', showAlbum)  
+    })   
 }
 
 const showAlbum = (event) => {
-    // no default action on a click
-    console.log(event.target.dataset.id)  //click event
     let band = document.getElementById('band')
-    let ul = document.getElementById('Album-List')
+    let rhcpList = document.getElementById('Album-List')
     
-    ul.innerHTML = ''
+    rhcpList.innerHTML = ''
     
-    fetch('http://localhost:3000' + `/Albums/${event.target.dataset.id}`) //this was a pain in the ass... returns promised obj
+    
+    fetch('http://localhost:3000' + `/Albums/${event.target.dataset.id}`) 
     .then(response => response.json())
     .then(Albums => {
-        console.log(Albums)
         band.innerHTML = 
         `<h1>${Albums.title}</h1><br/>
-        <h2>Album-Cover</h2>
+        <h2>Album Cover Art</h2>
         <img src="${Albums.url}"/>
-        <h2>Year-Album-Release</h2>
+        <h2>Year Album Release</h2>
         <h3>${Albums.year}</h3>
-        <h2>Album-Background-Information</h2>
+        <h2>Album Background Information</h2>
         <p>${Albums.description}</p>
-        <h2>Band-Members</h2>
+        <h2>Band Members</h2>
         <p>${Albums.members}</p>
         <h2>Songs</h2>
         <p>${Albums.songs.join(", ")}</p>`
     })
     }
 
- 
-
 let i = 0;
 let images = [];
 let time = 4000;
-
-
     images[0] = 'https://upload.wikimedia.org/wikipedia/en/5/5e/Rhcp1.jpg';
     images[1] = 'https://upload.wikimedia.org/wikipedia/en/5/5b/Freakystyleyalbumcover.jpg';
     images[2] = 'https://upload.wikimedia.org/wikipedia/en/a/a2/UpliftMofoCover.jpg';
@@ -77,18 +66,16 @@ let time = 4000;
     images[8] = 'https://upload.wikimedia.org/wikipedia/en/e/e6/Stadiumarcadium.jpg';
 
 function slideShow(){
-    document.slide.src = images[i];
+   document.slide.src = images[i];
 
         if(i < images.length - 1){
            i++;
         } else{
         i = 0
         }
-
  setTimeout("slideShow()", time);
 
 }
-
 window.onload = slideShow;
 
 
